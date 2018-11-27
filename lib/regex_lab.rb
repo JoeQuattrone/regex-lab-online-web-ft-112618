2f6662410b89cc29f5938f4fa26cb2729f994c3f
Joe Quattrone <joequattrone5@gmail.com>
2018-11-27 14:51:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   binding.pry
-  text.match(/^[a-z][A-Z]\$/) != nil ? true : false
+  text.match(/^[A-Z]\$/) != nil ? true : false
 end
 
 def valid_phone_number?(phone)
