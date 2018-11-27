d0a15e6222beff62ff0fff1be880b7448d6d11a1
Joe Quattrone <joequattrone5@gmail.com>
2018-11-27 14:37:09 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,16 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.match(/^[aeiouAEIOU]/) != nil ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  binding.pry
+  text.scan(/\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
