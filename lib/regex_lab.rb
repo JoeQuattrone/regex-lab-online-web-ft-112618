295cf33823f7f68ca9fd60d2c06edfe289dd0df5
Joe Quattrone <joequattrone5@gmail.com>
2018-11-27 15:33:52 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,5 +17,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-phone.match(/\d{10}/) != nil ? true : false
+phone.match(^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$
+) != nil ? true : false
 end
